579a58805ac8024a5898809732e7db3eeb86c7bf
Davi Augusto <davi_augusto11@hotmail.com>
2022-01-02 16:37:25 -0300

    File UsedProduct moddified with extend for products, word super, constructor and variable type date

--- Classes/UsedProduct.java
+++ Classes/UsedProduct.java
@@ -1,7 +1,12 @@
 package Products.Classes;
-
+import java.util.Date;
 public class UsedProduct extends Products{
+    private Date manufactureDate;
 
-    
-    
+    public UsedProduct(){
+        super();
+    }
+    public UsedProduct(String name, Double price){
+
+    }
 }
