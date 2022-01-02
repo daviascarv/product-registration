0ba3e1dc1dea66ca118ac43d923156e26c6280d8
Davi Augusto <davi_augusto11@hotmail.com>
2022-01-02 12:58:49 -0300

    Class of product imported modified: added variables, methods, constructors and getters & setters

--- Classes/ImportedProduct.java
+++ Classes/ImportedProduct.java
@@ -1,8 +1,31 @@
 package Products.Classes;
 
+
 public class ImportedProduct extends Products{
     private Double customsFee;
 
+    public ImportedProduct(){
+        super();
+    }
+    public ImportedProduct(String name, Double price, Double customsFee){
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+    public double totalPrice(){
+        return getPrice() + customsFee;
+    }
+    public String priceTag(){
+        return getName()
+
+                    +"(imported)"
+                    +"$";
+    }
 
     
     
